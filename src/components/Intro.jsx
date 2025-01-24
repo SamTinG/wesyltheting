f51785beda9f41d8ec9a8d6eea90c42ea2f556de
SamTing <tendousoujiooz@gmail.com>
2025-01-24 14:53:12 +0800

    fixes

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -9,7 +9,7 @@ const Intro = ({ onFinish }) => {
   return (
     <div style={{ height: '100vh', backgroundColor: 'black' }}>
       <video autoPlay muted style={{ width: 'auto', height: '100%', objectFit: 'cover' }}>
-        <source src={`https://drive.google.com/uc?export=download&id=11MI-TnEvqKKbxufjH7gCAYIu7_YSjxCn`} type="video/mp4" />
+        <source src={`${process.env.PUBLIC_URL}/intro.mp4`} type="video/mp4" />
       </video>
     </div>
   );
